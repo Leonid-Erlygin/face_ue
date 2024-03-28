c6e2a3c141f4a051995696e1657f0a47c5cb6213
leonid <erlygin.la@phystech.edu>
2024-03-28 09:07:45 +0000

    baseline

--- evaluation/open_set_methods/score_function_based.py
+++ evaluation/open_set_methods/score_function_based.py
@@ -3,6 +3,7 @@ from .base_method import OpenSetMethod
 from evaluation.open_set_methods.posterior_prob_based import PosteriorProb
 from scipy import interpolate
 
+
 class SimilarityBasedPrediction(OpenSetMethod):
     def __init__(
         self,
@@ -25,28 +26,32 @@ class SimilarityBasedPrediction(OpenSetMethod):
         self.T = T
         self.T_data_unc = T_data_unc
 
-    def setup(self,         probe_feats: np.ndarray,
+    def setup(
+        self,
+        probe_feats: np.ndarray,
         probe_unc: np.ndarray,
         gallery_feats: np.ndarray,
         gallery_unc: np.ndarray,
         g_unique_ids: np.ndarray,
-        probe_unique_ids: np.ndarray,):
+        probe_unique_ids: np.ndarray,
+    ):
         probe_feats = probe_feats[:, np.newaxis, :]
         self.data_uncertainty = probe_unc
 
         similarity_matrix = self.distance_function(
-                probe_feats,
-                probe_unc,
-                gallery_feats,
-                gallery_unc,
-            )
+            probe_feats,
+            probe_unc,
+            gallery_feats,
+            gallery_unc,
+        )
         self.similarity_matrix = np.mean(similarity_matrix, axis=1)
         self.probe_score = self.acceptance_score(self.similarity_matrix)
-        taus = np.linspace(0, 1, num=100)
+
         is_seen = np.isin(probe_unique_ids, g_unique_ids)
-        fars = [np.mean((self.probe_score < tau)[~is_seen] == False) for tau in taus]
-        f = interpolate.interp1d(fars, taus)
-        self.tau = f([self.far])[0]
+        out_of_gallery_scores = self.probe_score[~is_seen]
+        self.tau = np.sort(out_of_gallery_scores)[
+            int(out_of_gallery_scores.shape[0] * (1 - self.far))
+        ]
 
     def predict(self):
         predict_id = np.argmax(self.similarity_matrix, axis=-1)
@@ -65,7 +70,9 @@ class SimilarityBasedPrediction(OpenSetMethod):
 
         min_kappa = 150
         max_kappa = 2700
-        data_uncertainty_norm = (self.data_uncertainty - min_kappa) / (max_kappa - min_kappa)
+        data_uncertainty_norm = (self.data_uncertainty - min_kappa) / (
+            max_kappa - min_kappa
+        )
         assert np.sum(data_uncertainty_norm < 0) == 0
         # data_uncertainty_norm = data_uncertainty
         data_conf_norm = (data_uncertainty_norm) ** (1 / self.T_data_unc)
