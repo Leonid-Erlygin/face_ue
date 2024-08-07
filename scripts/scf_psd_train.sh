c243b2590881fa6c6c7b70f007b6f363cc3b8de9
vanekek <kolesnikov.ia@phystech.edu>
2024-08-07 16:07:10 +0000

    Augmentations were added to config

--- scripts/scf_psd_train.sh
+++ scripts/scf_psd_train.sh
@@ -7,10 +7,10 @@ docker run \
  --env WANDB_API_KEY=fc366b0c6dc3150de383b028451e1cfa35009932 \
  --rm \
  --init \
- -v /home/l.erlygin/face-evaluation:/app \
+ -v /home/i.kolesnikov/face_ue:/app \
  --gpus '"device=1"' \
  -w="/app" \
  kolesnikov-face \
  python trainers/train.py fit \
  --config configs/train/train_scf_with_psd.yaml \
- --ckpt_path=/app/outputs/scf_new_data/pixel_dropout/weights_pixel_drop_album_50%/epoch=11-step=174672_pretrained.ckpt
+ --ckpt_path=/app/outputs/scf_new_data/pixel_dropout/weights_pixel_drop_album_75%/epoch=11-step=174672_pretrained.ckpt
