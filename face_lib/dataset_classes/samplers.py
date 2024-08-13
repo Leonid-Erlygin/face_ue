3b8e26b01df4e9b0cc6858c4b9870eddb0e3181e
leonid <erlygin.la@phystech.edu>
2024-08-13 15:28:01 +0000

    new sampler

--- face_lib/dataset_classes/samplers.py
+++ face_lib/dataset_classes/samplers.py
@@ -2,6 +2,55 @@ from torch.utils.data.sampler import Sampler
 import numpy as np
 
 
+class UniformBatchSamplerWithBins(Sampler):
+    r"""Yield a mini-batch of indices.
+
+    Args:
+        cosine_sim_path: Path to computer cosine similarities of each sample to its class center
+        batch_size: Size of mini-batch.
+    """
+
+    def __init__(self, cosine_sim_path, batch_size, cosine_border_values: list):
+        self.batch_size = batch_size
+        cosine_sim = np.load(cosine_sim_path)
+        self.sorted_id_map = np.argsort(cosine_sim)
+        sorted_cosine_sim = np.sort(cosine_sim)
+        n_bins = len(cosine_border_values)
+        self.bin_sample_size = batch_size / n_bins
+        self.id_borders = []
+        id_widths = []
+        prev_border = 0
+        for cosine_border_values in cosine_border_values:
+            id_border = np.searchsorted(sorted_cosine_sim, cosine_border_values)
+            self.id_borders.append(id_border)
+            id_widths.append(id_border - prev_border)
+            prev_border = id_border
+        assert np.searchsorted(sorted_cosine_sim, 1) == sorted_cosine_sim.shape[0]
+
+        self.rng = np.random.default_rng(776)
+        self.ids = np.arange(sorted_cosine_sim.shape[0])
+
+    def __iter__(self):
+        # implement logic of sampling here
+        for _ in range(int(len(self.ids) // self.batch_size)):
+            sorted_batch_ids = []
+            prev_id = 0
+            for id_border in self.id_borders:
+                sorted_batch_ids.append(
+                    self.rng.choice(
+                        np.arange(prev_id, id_border),
+                        int(self.bin_sample_size),
+                    )
+                )
+                prev_id = id_border
+            sorted_batch_ids = np.concatenate(sorted_batch_ids)
+            uniform_batch_ids = self.sorted_id_map[sorted_batch_ids]
+            yield uniform_batch_ids
+
+    def __len__(self):
+        return int(len(self.ids) // self.batch_size)
+
+
 class UniformBatchSampler(Sampler):
     r"""Yield a mini-batch of indices.
 
--- scripts/pred_emp_scf.sh
+++ scripts/pred_emp_scf.sh
@@ -1,7 +1,7 @@
 docker run \
  --shm-size=8g \
  --memory=160g \
- --gpus '"device=1"' \
+ --gpus '"device=0"' \
  --cpus=40 \
  --user 1005:1005 \
  --name scf_pred \
@@ -12,5 +12,6 @@ docker run \
  face-eval \
  python trainers/train.py predict \
  --config configs/train/train_scf.yaml \
- --ckpt_path=/app/model_weights/scf_base.ckpt \
+ --ckpt_path=/app/outputs/scf_uniform_batch/default_scf/epoch=15-step=232896.ckpt
+# --ckpt_path=/app/model_weights/scf_base.ckpt \
  --trainer.devices=1
