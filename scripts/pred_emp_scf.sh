cbf2379ad3f9343a4168d7d72935e06d8261149b
leonid <erlygin.la@phystech.edu>
2024-08-16 08:35:58 +0000

    some experiments

--- scripts/pred_emp_scf.sh
+++ scripts/pred_emp_scf.sh
@@ -1,7 +1,7 @@
 docker run \
  --shm-size=8g \
  --memory=160g \
- --gpus '"device=1"' \
+ --gpus '"device=2"' \
  --cpus=40 \
  --user 1005:1005 \
  --name scf_pred \
@@ -12,5 +12,5 @@ docker run \
  face-eval \
  python trainers/train.py predict \
  --config configs/train/train_scf.yaml \
- --ckpt_path=/app/outputs/scf_uniform_batch/default_scf/epoch=0-step=14556.ckpt \
+ --ckpt_path=/app/outputs/scf_uniform_batch/default_scf/epoch=14-step=218340.ckpt \
 # --ckpt_path=/app/model_weights/scf_base.ckpt \
