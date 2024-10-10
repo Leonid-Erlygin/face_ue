b7b81fed9a519f8b447e67d87aaea47c46c6712d
leonid <erlygin.la@phystech.edu>
2024-10-10 15:31:27 +0000

    blur train

--- scripts/scf_train.sh
+++ scripts/scf_train.sh
@@ -11,7 +11,7 @@ docker run \
  --gpus '"device=1"' \
  -w="/app" \
  ${USER}_$(basename $(dirname "$PWD")) \
- python3 trainers/train_multiple_runs_scf.py -cn=train_scf_base
+ python3 trainers/train_multiple_runs_scf.py -cn=train_scf_blur #train_scf_base
 #  --config configs/train/train_scf_with_psd.yaml \
 #  --ckpt_path=/app/outputs/scf_new_data/vgg_old_backbone_from_zero/epoch=11-step=174672_pretrained.ckpt
 #--multirun data.train_dataset.torch_augments.0.init_args.sigma=2,3,4,5,7,9,11
