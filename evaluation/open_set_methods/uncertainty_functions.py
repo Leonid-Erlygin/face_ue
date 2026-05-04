12ad6e783fa3c99f2290685e2a3171b5f692a157
leonid <erlygin.la@phystech.edu>
2026-05-04 19:34:14 +0000

    new score

--- evaluation/open_set_methods/uncertainty_functions.py
+++ evaluation/open_set_methods/uncertainty_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import softmax
 
 from typing import Any
 
@@ -9,6 +10,14 @@ class BernoulliVariance:
         conf_score = np.abs(s - tau)
         return -conf_score
 
+class MaximumSoftmaxProbability:
+    def __init__(self, T: float):
+        self.T = T
+    def __call__(self, similarity: np.ndarray, probe_score: np.ndarray, tau: float) -> Any:
+        sims = np.column_stack([similarity, np.full(similarity.shape[0], tau)])
+        sims /= self.T
+        conf_score = np.max(softmax(sims, axis=-1), axis=-1)
+        return 1 - conf_score
 
 class RandomScore:
     def __call__(self, similarity: np.ndarray, probe_score: np.ndarray, tau) -> Any:
